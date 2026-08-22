7e3a7377ea1349ef5540b1e711b93f300c7334e9
LegendKiler <nadeem101@gmail.com>
2026-08-22 21:33:19 +1000

    LAB: Import themeForce + transparent App shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './themeForce.css';
 
 const params = new URLSearchParams(window.location.search);
 const isEmbed = Boolean(params.get('embed'));
